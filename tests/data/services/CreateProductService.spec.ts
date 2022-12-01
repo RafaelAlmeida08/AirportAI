5e31cfcb6ffd5aa60921d23a2377c733f2e70e5c
RafaelAlmeida08 <rafaelalmeida.08@hotmail.com>
2022-12-01 20:45:00 -0300

    ensure service throws if repository throws and return a product on success

--- tests/data/services/CreateProductService.spec.ts
+++ tests/data/services/CreateProductService.spec.ts
@@ -7,29 +7,54 @@ interface ICreateProductRepositoryStub {
 }
 
 class CreateProductRepositoryStub implements ICreateProductRepositoryStub {
-  async create (params: CreateProduct.Params): Promise<any> {}
+  async create (params: CreateProduct.Params): Promise<CreateProduct.Result> {
+    return {
+      _id: 'valid_id',
+      name: 'Bag',
+      color: 'black',
+      lostTime: new Date('2022-01-01'),
+      createdAt: new Date('2022-01-01')
+    }
+  }
 }
 
 describe('CreateProductService', () => {
-  let createProductRepositoryStub: any
+  let createProductRepositoryStub: CreateProductRepositoryStub
   let sut: CreateProductService
+  const fakeProductData = {
+    name: 'Bag',
+    color: 'black',
+    lostTime: new Date('2022-01-01')
+  }
   beforeEach(() => {
     createProductRepositoryStub = new CreateProductRepositoryStub()
     sut = new CreateProductService(createProductRepositoryStub)
   })
 
   it('Should call CreateProductRepository with correct params', async () => {
-    const productData = {
-      name: 'Bag',
-      color: 'black',
-      lostTime: new Date('2022-01-01')
-    }
     const createProductRepositorySpy = jest.spyOn(createProductRepositoryStub, 'create')
-    await sut.execute(productData)
+    await sut.execute(fakeProductData)
     expect(createProductRepositorySpy).toHaveBeenCalledWith({
       name: 'Bag',
       color: 'black',
       lostTime: new Date('2022-01-01')
+    })
+  })
+
+  it('Should throw if CreateProductRepository throws', async () => {
+    jest.spyOn(createProductRepositoryStub, 'create').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.execute(fakeProductData)
+    await expect(promise).rejects.toThrow()
+  })
+
+  it('Should return a Product on success', async () => {
+    const result = await sut.execute(fakeProductData)
+    expect(result).toEqual({
+      _id: 'valid_id',
+      name: 'Bag',
+      color: 'black',
+      lostTime: new Date('2022-01-01'),
+      createdAt: new Date('2022-01-01')
     })
   })
 })
