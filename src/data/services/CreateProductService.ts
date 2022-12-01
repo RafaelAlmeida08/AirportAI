036706c7b19197282648b7ca362406c509493779
RafaelAlmeida08 <rafaelalmeida.08@hotmail.com>
2022-12-01 20:47:47 -0300

    refactor: return service method directly

--- src/data/services/CreateProductService.ts
+++ src/data/services/CreateProductService.ts
@@ -3,7 +3,6 @@ import { CreateProduct, ICreateProduct } from '@/domain/features'
 export class CreateProductService implements ICreateProduct {
   constructor (private readonly createProductRepository: any) {}
   async execute (params: CreateProduct.Params): Promise<CreateProduct.Result> {
-    const product = await this.createProductRepository.create(params)
-    return product
+    return await this.createProductRepository.create(params)
   }
 }
