961d950ec7d541080029d89cf7681fc102387d6a
RafaelAlmeida08 <rafaelalmeida.08@hotmail.com>
2022-12-03 00:13:40 -0300

    refactor: connect to mongodb before integration tests

--- tests/main/routes/CreateProductRoute.spec.ts
+++ tests/main/routes/CreateProductRoute.spec.ts
@@ -1,7 +1,19 @@
 import request from 'supertest'
 import app from '@/main/config/app'
+import { MongoHelper } from '@/infra/db/mongodb/helpers'
 
 describe('CreateProductRoute', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect('mongodb://127.0.0.1:27017/AirportAI')
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  beforeEach(async () => {
+    const productCollection = MongoHelper.getCollection('products')
+    await productCollection.deleteMany({})
+  })
+
   it('Should return a product on success', async () => {
     await request(app)
       .post('/products')
