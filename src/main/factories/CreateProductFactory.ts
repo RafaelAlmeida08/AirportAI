25aff0af10001a97444e96c76ffe5f2274ec70fd
RafaelAlmeida08 <rafaelalmeida.08@hotmail.com>
2022-12-04 12:02:58 -0300

    feat: ensure LogControllerDecorator calls internal controller with correct values

--- src/main/factories/CreateProductFactory.ts
+++ src/main/factories/CreateProductFactory.ts
@@ -1,9 +1,12 @@
 import { CreateProductController } from '@/application/controllers'
 import { CreateProductService } from '@/data/services'
 import { CreateProductMongoRepository } from '@/infra/db/mongodb'
+import { Controller } from '@/presentation/protocols'
+import { LogControllerDecorator } from '@/main/decorators'
 
-export const makeCreateProductController = (): CreateProductController => {
+export const makeCreateProductController = (): Controller => {
   const createProductMongoRepository = new CreateProductMongoRepository()
   const createProductService = new CreateProductService(createProductMongoRepository)
-  return new CreateProductController(createProductService)
+  const createProductController = new CreateProductController(createProductService)
+  return new LogControllerDecorator(createProductController)
 }
