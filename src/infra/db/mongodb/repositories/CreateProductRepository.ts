0be5f644abab58480b60ac6de9bbbc0b0feabec5
RafaelAlmeida08 <rafaelalmeida.08@hotmail.com>
2022-12-05 21:35:46 -0300

    feat: add createdAt field

--- src/infra/db/mongodb/repositories/CreateProductRepository.ts
+++ src/infra/db/mongodb/repositories/CreateProductRepository.ts
@@ -5,6 +5,12 @@ import { MongoHelper } from '../helpers'
 export class CreateProductMongoRepository implements ICreateProductRepository {
   async create (params: CreateProduct.Params): Promise<any> {
     const productCollection = MongoHelper.getCollection('products')
-    return await productCollection.insertOne(params)
+    return await productCollection.insertOne({
+      name: params.name,
+      color: params.color,
+      description: params.description,
+      lostTime: params.lostTime,
+      createdAt: new Date()
+    })
   }
 }
