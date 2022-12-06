9bce82689660f8f2d4504b1d24f16d5de6b2a977
RafaelAlmeida08 <rafaelalmeida.08@hotmail.com>
2022-12-05 21:08:57 -0300

    refactor: create optimized field validation check

--- src/application/controllers/CreateProductController.ts
+++ src/application/controllers/CreateProductController.ts
@@ -4,27 +4,20 @@ import { badRequest, created, serverError } from '@/presentation/helpers'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
 export class CreateProductController implements Controller {
-  private readonly createProductService: ICreateProduct
-  constructor (createProductService: ICreateProduct) {
-    this.createProductService = createProductService
-  }
+  constructor (private readonly createProductService: ICreateProduct) {}
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
     try {
+      const requiredFields = ['name', 'color', 'description', 'lostTime']
+      for (const field of requiredFields) {
+        if (!request.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
+
       const { name, color, description, lostTime } = request.body
-      if (!name) {
-        return badRequest(new MissingParamError('name'))
-      }
-      if (!color) {
-        return badRequest(new MissingParamError('color'))
-      }
-      if (!description) {
-        return badRequest(new MissingParamError('name'))
-      }
-      if (!lostTime) {
-        return badRequest(new MissingParamError('lostTime'))
-      }
       const product = await this.createProductService.execute({ name, color, description, lostTime })
+
       return created(product)
     } catch (error) {
       return serverError(error as Error)
